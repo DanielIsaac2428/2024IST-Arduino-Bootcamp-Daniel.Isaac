bae652c0f27c29b7c1f2e9fe2d3dcd420ed93b4f
Daniel <daniel.isaac16@education.nsw.gov.au>
2023-08-23 11:44:46 +1000

    Update 06.pulseWidthModulation.ino

--- 06.pulseWidthModulation/06.pulseWidthModulation.ino
+++ 06.pulseWidthModulation/06.pulseWidthModulation.ino
@@ -16,10 +16,25 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/06.pulseWidthModulation/Bootcamp-PWMOutput.png
 */
 
-void setup() {
+// allocate pin for LED 
+unsigned static int myLED = 3;
 
+void setup() {
+  // setup and debug serial monitor
+Serial.begin(9600);
+Serial.println("SM Working");
+Serial.println("-----------------");
+
+//configure pin out
+pinMode(myLED, OUTPUT);
 }
 
 void loop() {
+
+for(int i = 0; i <= 255; i++) {
+analogWrite(myLED,i);
+Serial.println(i);
+delay(10);
+}
 
 }
